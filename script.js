08112badce629cf30b59411b2e4616fcc64fb9ed
Zoe Smith <107585629+ZoeGrey86@users.noreply.github.com>
2022-07-06 23:45:43 -0500

    all but the bonus sorting

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // TODO: add code here
+
+window.addEventListener("load", function(){
+ fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+     response.json().then(function(json){
+         const container = document.getElementById("container");
+         let count = document.getElementById("h1");
+         count.innerHTML += `${json.length}`;
+         let status =" ";
+         for(i=0; i<json.length; i++){
+                if(json[i].active === true){status = "active-true" };
+                container.innerHTML += `
+             <div class="astronaut">
+                 <div class="bio">
+                     <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                     <ul>
+                         <li>Hours in space: ${json[i].hoursInSpace}</li>
+                         <li>Active:<span class = "${status}">${json[i].active}</span></li>
+                         <li>Skills: ${json[i].skills}</li>
+                     </ul>
+                    </div>
+                  <img class="avatar" src="${json[i].picture}">
+             </div>`
+            };
+        });
+    });
+});
