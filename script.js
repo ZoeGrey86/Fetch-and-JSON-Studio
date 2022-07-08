bf818aad34bcc38451a5fcf59f322f07e63eab2c
Zoe Smith <107585629+ZoeGrey86@users.noreply.github.com>
2022-07-08 13:15:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,17 @@
 window.addEventListener("load", function(){
  fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
      response.json().then(function(json){
+        console.log(json)
          const container = document.getElementById("container");
          let count = document.getElementById("h1");
          count.innerHTML += `${json.length}`;
          let status =" ";
+         let arr = [];
          for(i=0; i<json.length; i++){
+            
+            let hrsInSpc = json[i].hoursInSpace;
+            arr.push(hrsInSpc)
+             arr.push('heellloo')
                 if(json[i].active === true){status = "active-true" };
                 container.innerHTML += `
              <div class="astronaut">
@@ -21,7 +27,9 @@ window.addEventListener("load", function(){
                     </div>
                   <img class="avatar" src="${json[i].picture}">
              </div>`
+console.log(arr)
             };
+            
         });
     });
 });
